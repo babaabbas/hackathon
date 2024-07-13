557b1fb7d3d3d3683d6950101e6fc258fbf09329
babaabbasCool <59871320+babaabbas@users.noreply.github.com>
2024-07-13 23:08:59 +0530

    ragg is working it will will give output in telegram bot

--- ragg.py
+++ ragg.py
@@ -1,20 +1,10 @@
-import getpass
 import os
-#hello
-if "HUGGINGFACEHUB_API_TOKEN" not in os.environ:
-    os.environ["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"] = getpass.getpass("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")
+from langchain_community.document_loaders import TextLoader
 
-import requests
 
-url = "https://raw.githubusercontent.com/hwchase17/chat-your-data/master/state_of_the_union.txt"
-res = requests.get(url)
-with open("state_of_the_union.txt", "w") as f:
-  f.write(res.text)
-# Document Loader
-from langchain.document_loaders import TextLoader
-loader = TextLoader('./state_of_the_union.txt')
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+loader = TextLoader('/Users/abbasbaba/PycharmProjects/hackathon/abs.txt')
 documents = loader.load()
-
 import textwrap
 
 def wrap_text_preserve_newlines(text, width=110):
@@ -28,33 +18,37 @@ def wrap_text_preserve_newlines(text, width=110):
     wrapped_text = '\n'.join(wrapped_lines)
 
     return wrapped_text
-print(wrap_text_preserve_newlines(str(documents[0])))
-# Text Splitter
+
+
 from langchain.text_splitter import CharacterTextSplitter
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(documents)
 
-# Embeddings
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
 
 embeddings = HuggingFaceEmbeddings()
-# Vectorstore: https://python.langchain.com/en/latest/modules/indexes/vectorstores.html
+
 from langchain.vectorstores import FAISS
 
 db = FAISS.from_documents(docs, embeddings)
-query = "What did the president say about the Supreme Court"
+print(wrap_text_preserve_newlines(str(docs[0].page_content)))
+query = "who is speaking here"
 docs = db.similarity_search(query)
 from langchain.chains.question_answering import load_qa_chain
 from langchain import HuggingFaceHub
 from langchain_community.llms import HuggingFaceEndpoint
 
-
 llm=HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2", temperature=0.1, max_length=512)
 chain = load_qa_chain(llm, chain_type="stuff")
-query = "What did the president say about the Supreme Court"
+query = "what is this"
 docs = db.similarity_search(query)
-chain.run(input_documents=docs, question=query)
-query = "What did the president say about economy?"
-docs = db.similarity_search(query)
-chain.run(input_documents=docs, question=query)
+result=chain.run({"input_documents":docs, "question":query})
+print(result)
+def query_answer(query):
+    result = chain.run({"input_documents": docs, "question": query})
+    return result
+
+
+
 
